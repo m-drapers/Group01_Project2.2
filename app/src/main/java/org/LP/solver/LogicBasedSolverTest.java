661abba5727ded6b498bcd5b39e10c735f40b452
RW8440 <r.withaar@student.maastrichtuniversity.nl>
2024-05-27 21:29:20 +0200

    New version of the solver

--- app/src/main/java/org/LP/solver/LogicBasedSolverTest.java
+++ app/src/main/java/org/LP/solver/LogicBasedSolverTest.java
@@ -5,13 +5,16 @@ public class LogicBasedSolverTest {
     public static void main(String[] args) {
         double[][] A = { { 300, 25 },
                 { 325, 40 },
-                {400, 6 },
-                { 400, 35 }
+                {400, 7 },
+                { 400, 35 },
+                { 450, 5 },
+                {450, 4 },
+                { 450, 30 },
         };
         double[][] B = {{275, 10, 1},
                 {300, 20, 2},
-                {400, 6, 1},
-                {400, 30, 1}
+                {400, 5, 1},
+                {400, 6, 3}
         };
 
         LogicBasedSolver logicBasedSolver = new LogicBasedSolver();
