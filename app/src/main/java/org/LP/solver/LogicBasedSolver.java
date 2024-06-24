24bd7e0518f7c7c799962375b21510a34150f0c4
RW8440 <r.withaar@student.maastrichtuniversity.nl>
2024-06-24 18:16:51 +0200

    Genetic Solver

--- app/src/main/java/org/LP/solver/LogicBasedSolver.java
+++ app/src/main/java/org/LP/solver/LogicBasedSolver.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class LogicBasedSolver {
 
-    private static double[][] students; // Sorted students matrix based on budget low to high, then on max distance low to high
-    private static double[][] houses; // Sorted houses matrix based on price low to high, then distance low to high
+    static double[][] students; //Sorted students matrix based on budget low to high, then on max distance low to high
+    static double[][] houses; //Sorted houses matrix based on price low to high, then distance low to high
 
     public double LogicBasedSolver(double[][] students, double[][] houses) {
         LogicBasedSolver.students = students;
@@ -29,7 +29,7 @@ public class LogicBasedSolver {
         double[] costs = new double[students.length];
         int[] placesTaken = new int[houses.length];
 
-        //fill the placesTaken array with zeros
+        //Fill the placesTaken array with zeros
         Arrays.fill(placesTaken, 0);
 
         int houseNumber = 0;
@@ -70,10 +70,7 @@ public class LogicBasedSolver {
                 //Check if the student can be placed in a house which is already full
                 //Then we have to reallocate some other student(s)
                 for (int j = 0; j < i; j++) {
-                    System.out.println(i);
-                    System.out.println(j);
-                    System.out.println(solution[j]);
-                    if (students[j][0] >= houses[houseNumber][0] && students[j][1] >= houses[houseNumber][1] && students[i][1] >= houses[j][1] && violationDistance) {
+                    if (students[j][0] >= houses[houseNumber][0] && students[j][1] >= houses[houseNumber][1] && students[i][1] >= houses[solution[j]][1] && violationDistance) { //One mistake here
                         newHouseNumber = solution[j];
                         solution[j] = houseNumber;
                         costs[j] = houses[houseNumber][0];
